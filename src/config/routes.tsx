be46184b048f9901c8906a2e0aba9a4d79e26264
kdkocev <kamen@hacksoft.io>
2022-07-07 14:50:18 +0300

    Add navigation

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,4 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  NavLink,
+} from "react-router-dom";
+import _ from "lodash";
+
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 import { URLS } from "config/urls";
 
@@ -10,32 +20,53 @@ import MapExampleNoKey from "pages/MapExampleNoKey";
 import MapExampleNoKeyReorder from "pages/MapExampleNoKeyReorder";
 import MultipleFieldsNoMap from "pages/MultipleFieldsNoMap";
 
+const PageLayout = () => {
+  return (
+    <>
+      <Stack spacing={1} direction="row">
+        {_.map(URLS, (url, key) => (
+          <NavLink to={url} key={key}>
+            {({ isActive }) => (
+              <Button variant={isActive ? "contained" : "text"}>
+                {key.toLowerCase().replaceAll("_", " ")}
+              </Button>
+            )}
+          </NavLink>
+        ))}
+      </Stack>
+      <Outlet />
+    </>
+  );
+};
+
 const LocalRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path={URLS.MUI_FIELDS_UNCONTROLLED}
-          element={<MuiFieldsUncontrolled />}
-        />
-        <Route
-          path={URLS.MUI_FIELDS_CONTROLLED}
-          element={<MuiFieldsControlled />}
-        />
-        <Route path={URLS.MUI_FIELDS_ON_BLUR} element={<MuiFieldsOnBlur />} />
-        <Route
-          path={URLS.MUI_FIELDS_ON_CHNAGE_DEBOUNCE}
-          element={<MuiFieldsOnChangeDebounce />}
-        />
-        <Route path={URLS.MAP_EXAMPLE_NO_KEY} element={<MapExampleNoKey />} />
-        <Route
-          path={URLS.MAP_EXAMPLE_NO_KEY_REORDER}
-          element={<MapExampleNoKeyReorder />}
-        />
-        <Route
-          path={URLS.MULTIPLE_FIELDS_NO_MAP}
-          element={<MultipleFieldsNoMap />}
-        />
+        <Route path="/" element={<PageLayout />}>
+          <Route
+            path={URLS.MUI_FIELDS_UNCONTROLLED}
+            element={<MuiFieldsUncontrolled />}
+          />
+          <Route
+            path={URLS.MUI_FIELDS_CONTROLLED}
+            element={<MuiFieldsControlled />}
+          />
+          <Route path={URLS.MUI_FIELDS_ON_BLUR} element={<MuiFieldsOnBlur />} />
+          <Route
+            path={URLS.MUI_FIELDS_ON_CHNAGE_DEBOUNCE}
+            element={<MuiFieldsOnChangeDebounce />}
+          />
+          <Route path={URLS.MAP_EXAMPLE_NO_KEY} element={<MapExampleNoKey />} />
+          <Route
+            path={URLS.MAP_EXAMPLE_NO_KEY_REORDER}
+            element={<MapExampleNoKeyReorder />}
+          />
+          <Route
+            path={URLS.MULTIPLE_FIELDS_NO_MAP}
+            element={<MultipleFieldsNoMap />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
